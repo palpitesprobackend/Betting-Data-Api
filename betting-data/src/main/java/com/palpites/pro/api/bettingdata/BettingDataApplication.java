7aec16a8704fe99b52d32b3bf36ddb778d535e69
Bruno Jose Pernambuco Santiago <brunopernambucope@gmail.com>
2023-08-16 18:07:32 -0300

    projeto inicial

---
+++ betting-data/src/main/java/com/palpites/pro/api/bettingdata/BettingDataApplication.java
@@ -0,0 +1,13 @@
+package com.palpites.pro.api.bettingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingDataApplication.class, args);
+	}
+
+}
---
+++ betting-data/src/test/java/com/palpites/pro/api/bettingdata/BettingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palpites.pro.api.bettingdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
